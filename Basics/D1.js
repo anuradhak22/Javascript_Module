4ded28f3ccfd701615a8ef1f4494965e0efdc9a9
anuradhak22 <140937263+anuradhak22@users.noreply.github.com>
2024-09-10 09:05:32 +0000

    Done

--- Basics/D1.js
+++ Basics/D1.js
@@ -1,1 +1,10 @@
 console.log("Hello");
+const accountId = 165446;
+console.log(accountId);
+var name = "Anuradha";
+a = 10;
+{
+    name = "Anushka";
+    console.log(name);
+}
+console.log(name);
---
+++ Basics/D2.js
@@ -0,0 +1,14 @@
+"use strict"; //use the code as newer version.
+// alert(3+3) we are using node js not browser.
+alert("hi")
+// Datatypes
+/*
+int 
+bigint
+string 
+boolean
+symbol
+undefined
+null
+console.log(typeof a);
+*/
