0789bc40eba02cec01bf6457c68f3820755865db
anuradhak22 <140937263+anuradhak22@users.noreply.github.com>
2024-09-12 10:30:27 +0000

    conversion and  datatype

---
+++ Basics/stringconversion.js
@@ -0,0 +1,20 @@
+console.log("1"+ 2)//12
+console.log(2+"1");//12
+console.log(1+2+"3");//33
+console.log("1"+ 2+3);//123
+console.log(+true)//1
+console.log(+"")//0
+let x = 3;
+x++;
+console.log(x);
+let y = 3
+++y;
+console.log(y);
+// comparison operator.
+console.log(null>0);//f
+console.log(null == 0);//f
+console.log(null>=0);//t
+// ===  check value with its datatype.
+console.log("2" === 2);
+
+
